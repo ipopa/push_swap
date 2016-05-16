990e82c22dd9577a4c87988978669d9d8f6597e3
ipopa <ipopa@student.42.fr>
2016-05-16 10:50:16 +0200

    push

--- srcs/check.c
+++ srcs/check.c
@@ -32,16 +32,31 @@ int		check_doublon(char **av)
 	return (0);
 }
 
+int		check_int(char *str)
+{
+  int i;
+  int cmp;
+
+  i = 0;
+  if (str[0] == '-')
+    str++;
+  if (ft_strcmp(str, "2147483647") > 0)
+    return 0;
+  return 1;
+}
+
 int		check_arg(int ac, char **av)
 {
-	int		i;
+  int		i;
 
-	i = 0;
-	while (i < ac)
-	{
-		if (ft_atoi(av[i]) == 0 && !(ft_strequ(av[i], "0")))
-			return (1);
-		i++;
-	}
-	return (0);
+  i = 0;
+  while (i < ac)
+    {
+      if (ft_atoi(av[i]) == 0 && !(ft_strequ(av[i], "0")))
+	return 1;
+      else if (check_int(av[i]) == 0)
+	return 1;
+      i++;
+    }
+  return 0;
 }
